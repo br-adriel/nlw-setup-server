c43afdac035368b150aa83ea9754a435b342be1b
Adriel <adriel.fsantos@outlook.com>
2023-01-17 22:32:42 -0300

    Implementa rota de listagem de hábitos do dia

--- src/routes.ts
+++ src/routes.ts
@@ -4,6 +4,48 @@ import { z } from 'zod';
 import { prisma } from './lib/prisma';
 
 export async function appRoutes(app: FastifyInstance) {
+  app.get('/day', async (req) => {
+    const getDayParams = z.object({
+      date: z.coerce.date(),
+    });
+
+    const { date } = getDayParams.parse(req.query);
+
+    const parsedDate = dayjs(date).startOf('day');
+    const weekDay = parsedDate.get('day');
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date,
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay,
+          },
+        },
+      },
+    });
+
+    const day = await prisma.day.findFirst({
+      where: {
+        date: parsedDate.toDate(),
+      },
+      include: {
+        dayHabits: true,
+      },
+    });
+
+    const completedHabits = day?.dayHabits.map((dayHabit) => {
+      return dayHabit.habit_id;
+    });
+
+    return {
+      possibleHabits,
+      completedHabits,
+    };
+  });
+
   app.post('/habits', async (req) => {
     const createHabitBody = z.object({
       title: z.string(),
