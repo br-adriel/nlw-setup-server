ef2e9da3c77c761a349552529cb82eab79760709
Adriel <adriel.fsantos@outlook.com>
2023-01-19 17:26:57 -0300

    Implementa rota de toogle do hábito

--- src/routes.ts
+++ src/routes.ts
@@ -70,4 +70,51 @@ export async function appRoutes(app: FastifyInstance) {
       },
     });
   });
+
+  app.patch('/habits/:id/toggle', async (req) => {
+    const toogleHabitParams = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = toogleHabitParams.parse(req.params);
+    const today = dayjs().startOf('day').toDate();
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today,
+      },
+    });
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today,
+        },
+      });
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      },
+    });
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id,
+        },
+      });
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id,
+        },
+      });
+    }
+  });
 }
